0bb34d3bd71494ccd8146c36f4f9e7224847b77a
AlexBrito <a.brito@financierasustentable.com>
2021-04-03 14:34:34 -0600

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '0kr!oqoi3_6p8n4x)+#d+m=^$8cmqlj5-9o85jylt820y)h@0j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
